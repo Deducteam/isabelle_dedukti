968837df2206a21be73f25e5ef4c62872aaf09a7
Makarius <makarius@sketis.net>
2019-06-21 11:22:53 +0200

    initial setup of Isabelle/Dedukti component

---
+++ src/dedukti.scala
@@ -0,0 +1,14 @@
+package isabelle.dedukti
+
+import isabelle._
+
+object Dedukti
+{
+  /* Isabelle tool wrapper */
+
+  val isabelle_tool =
+    Isabelle_Tool("dedukti_import", "import theory content into Dedukti", args =>
+    {
+      ???
+    })
+}
---
+++ src/tools.scala
@@ -0,0 +1,7 @@
+package isabelle.dedukti
+
+import isabelle._
+
+class Tools extends isabelle.Isabelle_Scala_Tools(
+  Dedukti.isabelle_tool
+)
