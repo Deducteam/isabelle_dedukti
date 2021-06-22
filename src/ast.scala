6f8c7c98122f8ec8a753e22cb06aa3a0c48a186f
Yann Leray <yann.leray@ens.psl.eu>
2021-06-23 01:43:03 +0200

    Add notation-less lambdapi output

--- src/ast.scala
+++ src/ast.scala
@@ -3,6 +3,7 @@
 
 package isabelle.dedukti
 
+import scala.annotation.tailrec
 import isabelle._
 
 
@@ -12,37 +13,66 @@ object Syntax
 
   sealed abstract class Term
   type Typ = Term
-  sealed case class Arg(id: Option[Ident], typ: Option[Typ])
+  sealed case class BoundArg(
+    id: Option[Ident],
+    typ: Option[Typ],
+    implicit_arg: Boolean = false)
 
   case object TYPE extends Term
   case  class Symb(id: Ident) extends Term
-  case  class FVar(id: Ident) extends Term
-  case  class BVar(idx: Int) extends Term
-  case  class Appl(t1: Term, t2: Term) extends Term
-  case  class Abst(arg: Arg, t: Term) extends Term
-  case  class Prod(arg: Arg, t: Term) extends Term
+  case  class  Var(id: Ident) extends Term
+  case  class Appl(t1: Term, t2: Term, isImplicit: Boolean = false) extends Term
+  case  class Abst(arg: BoundArg, t: Term) extends Term
+  case  class Prod(arg: BoundArg, t: Term) extends Term
 
-  def arrow(ty: Typ, tm: Term): Term = Prod(Arg(None, Some(ty)), tm)
+  def arrow(ty: Typ, tm: Term): Term = Prod(BoundArg(None, Some(ty)), tm)
 
-  def appls(head: Term, spine: List[Term]): Term = spine.foldLeft(head)(Appl)
-  def arrows(tys: List[Typ], tm: Term): Term =  tys.foldRight(tm)(arrow)
+  def appls(head: Term, spine: List[Term], impl: Boolean = false): Term = spine.foldLeft(head)(Appl(_, _, impl))
+  def arrows(tys: List[Typ], tm: Term): Term = tys.foldRight(tm)(arrow)
 
-  def dest_appls(t: Term, args: List[Term]): (Term, List[Term]) =
+  @tailrec
+  def destruct_appls(t: Term, args: List[(Term, Boolean)] = Nil): (Term, List[(Term, Boolean)]) =
     t match {
-      case Syntax.Appl(t1, t2) => dest_appls(t1, args = t2 :: args)
+      case Syntax.Appl(t1, t2, b) => destruct_appls(t1, args = (t2, b) :: args)
       case t => (t, args)
     }
 
-  sealed abstract class NotationArg
-  case class Prefix(priority: Number) extends NotationArg
-  case class InfixLeft(priority: Number) extends NotationArg
-  case class InfixRight(priority: Number) extends NotationArg
-  case class Quantifier() extends NotationArg
+  sealed abstract class Notation
+  case class Prefix(op: Ident, priority: Float) extends Notation
+  case class Infix (op: Ident, priority: Float) extends Notation
+  case class InfixL(op: Ident, priority: Float) extends Notation
+  case class InfixR(op: Ident, priority: Float) extends Notation
+  case class Quantifier(op: Ident) extends Notation
+
+  def getPriority(not: Notation): Option[Float] =
+    not match {
+      case Prefix(_, priority) => Some(priority)
+      case Infix (_, priority) => Some(priority)
+      case InfixL(_, priority) => Some(priority)
+      case InfixR(_, priority) => Some(priority)
+      case Quantifier(_) => None
+    }
+
+  def getOperator(not: Notation): Ident =
+    not match {
+      case Prefix(op, _) => op
+      case Infix (op, _) => op
+      case InfixL(op, _) => op
+      case InfixR(op, _) => op
+      case Quantifier(op) => op
+    }
+
+  val appNotation: Notation = InfixL(" ", Float.PositiveInfinity)
+  val justHadPars: Notation = Infix("()", Float.NegativeInfinity)
+  val arrNotation: Notation = InfixR("→", -2)
+  val absNotation: Notation = InfixR("λ", -1)
+  val defaultPrefixPriority = 10
 
   sealed abstract class Command
+  case class Declaration(id: Ident, args: List[BoundArg], ty: Typ, not: Option[Notation] = None) extends Command
+  case class DefableDecl(id: Ident, ty: Typ, inj: Boolean = false, not: Option[Notation] = None) extends Command
+  case class Definition (id: Ident, args: List[BoundArg], ty: Option[Typ],
+                          tm: Term, not: Option[Notation] = None) extends Command
+  case class Theorem(id: Ident, args: List[BoundArg], ty: Typ, prf: Term) extends Command
   case class Rewrite(vars: List[Ident], lhs: Term, rhs: Term) extends Command
-  case class Notation(id: Ident, arg: NotationArg) extends Command
-  case class Declaration(id: Ident, args: List[Arg], ty: Typ, const: Boolean = true) extends Command
-  case class Definition(id: Ident, args: List[Arg], ty: Option[Typ], tm: Term) extends Command
-  case class Theorem(id: Ident, args: List[Arg], ty: Typ, prf: Term) extends Command
 }
--- src/importer.scala
+++ src/importer.scala
@@ -10,7 +10,7 @@ object Importer
 {
   /* importer */
 
-  val default_output_file: Path = Path.explode("output.dk")
+  val default_output_file: Path = Path.explode("main.lp")
 
   def importer(
     options: Options,
@@ -18,8 +18,9 @@ object Importer
     progress: Progress = new Progress(),
     dirs: List[Path] = Nil,
     fresh_build: Boolean = false,
+    use_notations: Boolean = false,
     output_file: Path = default_output_file,
-    verbose: Boolean = false): Unit =
+    verbose: Boolean = false)
   {
     /* build session with exports */
 
@@ -177,7 +178,7 @@ object Importer
           for (name <- all_theories) {
             using(new Part_Writer(theory_file(name.theory)))(writer =>
             {
-              val syntax = new LP_Writer(output_file.dir, writer)
+              val syntax = new LP_Writer(output_file.dir, use_notations, writer)
               syntax.eta_equality()
 
               for {
@@ -192,7 +193,7 @@ object Importer
           // write one file that loads all the other ones
           using(new Part_Writer(output_file))(output =>
           {
-            val syntax = new LP_Writer(output_file.dir, output)
+            val syntax = new LP_Writer(output_file.dir, use_notations, output)
             all_theories.foreach(name => syntax.require_open(name.theory))
           })
 
@@ -211,6 +212,7 @@ object Importer
       var output_file = default_output_file
       var dirs: List[Path] = Nil
       var fresh_build = false
+      var use_notations = false
       var options = Options.init()
       var verbose = false
 
@@ -221,6 +223,7 @@ Usage: isabelle dedukti_import [OPTIONS] SESSION
     -O FILE      output file for Dedukti theory in dk or lp syntax (default: """ + default_output_file + """)
     -d DIR       include session directory
     -f           fresh build
+    -n           use lambdapi notations
     -o OPTION    override Isabelle system OPTION (via NAME=VAL or NAME)
     -v           verbose mode
 
@@ -229,6 +232,7 @@ Usage: isabelle dedukti_import [OPTIONS] SESSION
       "O:" -> (arg => output_file = Path.explode(arg)),
       "d:" -> (arg => { dirs = dirs ::: List(Path.explode(arg)) }),
       "f" -> (_ => fresh_build = true),
+      "n" -> (_ => use_notations = true),
       "o:" -> (arg => { options += arg }),
       "v" -> (_ => verbose = true))
 
@@ -251,6 +255,7 @@ Usage: isabelle dedukti_import [OPTIONS] SESSION
             progress = progress,
             dirs = dirs,
             fresh_build = fresh_build,
+            use_notations = use_notations,
             output_file = output_file,
             verbose = verbose)
         }
