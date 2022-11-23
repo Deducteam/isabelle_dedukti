032225a14ff60fa9ce2135b992bdcc9e3379c53a
Frédéric Blanqui <frederic.blanqui@inria.fr>
2022-11-23 17:11:46 +0100

    remove Tools

--- src/tools.scala
+++ src/tools.scala
@@ -1,14 +1,9 @@
 /** Isabelle command-line tools **/
 
-
 package isabelle.dedukti
 
 import isabelle._
 
-class Tools extends isabelle.Isabelle_Scala_Tools(
-  dedukti.Importer.isabelle_tool
-)
+//class Tools extends isabelle.Isabelle_Scala_Tools(dedukti.Importer.isabelle_tool)
 
-class Tools2 extends isabelle.Isabelle_Scala_Tools(
-  dedukti.Generator.isabelle_tool
-)
+class Tools2 extends isabelle.Isabelle_Scala_Tools(dedukti.Generator.isabelle_tool)
