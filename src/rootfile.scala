0f20d229adde234f671811ad2d4f5f0199a8e11d
Frédéric Blanqui <frederic.blanqui@inria.fr>
2022-11-25 12:50:41 +0100

    add optional theory argument to dedukti_root

--- src/rootfile.scala
+++ src/rootfile.scala
@@ -57,6 +57,7 @@ object Rootfile {
   def rootfile(
     options: Options,
     session: String,
+    target_theory : String,
     progress: Progress = new Progress(),
     dirs: List[Path] = Nil,
     verbose: Boolean = false,
@@ -74,20 +75,23 @@ object Rootfile {
     val file = new File(filename)
     val bw = new BufferedWriter(new FileWriter(file))
     var previous_session = "Pure"
-    for (theory <- theories.tail) {
-      val theory_name = theory.toString
-      val session_name = "Dedukti_" + theory_name
-      bw.write("session " + session_name + " in \"Ex/" + theory_name + "\" = " + previous_session + " +\n")
-      bw.write("   options [export_theory, export_proofs, record_proofs = 2]\n")
-      bw.write("   sessions\n")
-      bw.write("      " + session + "\n")
-      bw.write("   theories\n")
-      bw.write("      " + theory_name + "\n\n")
+    breakable {
+      for (theory <- theories.tail) {
+        val theory_name = theory.toString
+        val session_name = "Dedukti_" + theory_name
+        bw.write("session " + session_name + " in \"Ex/" + theory_name + "\" = " + previous_session + " +\n")
+        bw.write("   options [export_theory, export_proofs, record_proofs = 2]\n")
+        bw.write("   sessions\n")
+        bw.write("      " + session + "\n")
+        bw.write("   theories\n")
+        bw.write("      " + theory_name + "\n\n")
 
-      //if (!Files.exists(Paths.get("Ex/"+theory_name))) { }
-      "mkdir -p Ex/"+theory_name !
+        //if (!Files.exists(Paths.get("Ex/"+theory_name))) { }
+        "mkdir -p Ex/"+theory_name !
 
-      previous_session = session_name
+        previous_session = session_name
+        if (theory_name == target_theory) break()
+      }
     }
     bw.close()
 
@@ -97,7 +101,12 @@ object Rootfile {
     val file2 = new File(filename2)
     val bw2 = new BufferedWriter(new FileWriter(file2))
     bw2.write("#!/bin/sh\nkocheck --eta -j ${JOBS:-7} STTfa.dk")
-    for (theory <- theories) bw2.write(" " + Prelude.mod_name(theory.toString) + ".dk")
+    breakable {
+      for (theory <- theories) {
+        bw2.write(" " + Prelude.mod_name(theory.toString) + ".dk")
+        if (theory.toString == target_theory) break()
+      }
+    }
     bw2.write("\n")
     bw2.close()
 
@@ -107,7 +116,12 @@ object Rootfile {
     val file3 = new File(filename3)
     val bw3 = new BufferedWriter(new FileWriter(file3))
     bw3.write("#!/bin/sh\nfor f in STTfa.dk")
-    for (theory <- theories) bw3.write(" " + Prelude.mod_name(theory.toString) + ".dk")
+    breakable {
+      for (theory <- theories) {
+        bw3.write(" " + Prelude.mod_name(theory.toString) + ".dk")
+        if (theory.toString == target_theory) break()
+      }
+    }
     bw3.write("\ndo\n  dk check -e --eta $f\ndone\n")
     bw3.close()
   }
@@ -121,23 +135,24 @@ object Rootfile {
         var options = Options.init()
         var verbose = false
 
-        val getopts = Getopts("Usage: isabelle " + cmd_name + """ [OPTIONS] SESSION
+        val getopts = Getopts("Usage: isabelle " + cmd_name + """ [OPTIONS] SESSION [THEORY]
 
   Options are:
     -d DIR       include session directory
     -o OPTION    override Isabelle system OPTION (via NAME=VAL or NAME)
     -v           verbose mode
 
-Generate a ROOT file with a proof-exporting session named Dedukti_$theory for each $theory of SESSION.""",
+Generate a ROOT file with a proof-exporting session named Dedukti_$theory for each $theory of SESSION (up to THEORY), and the scripts kocheck.sh and dkcheck.sh to check dk files.""",
         "d:" -> (arg => { dirs = dirs ::: List(Path.explode(arg)) }),
         "o:" -> (arg => { options += arg }),
         "v" -> (_ => verbose = true))
 
         val more_args = getopts(args)
 
-        val session =
+        val (session, theory) =
           more_args match {
-            case List(session) => session
+            case List(session) => (session, "")
+            case List(session, theory) => (session, theory)
             case _ => getopts.usage()
           }
 
@@ -147,7 +162,7 @@ Generate a ROOT file with a proof-exporting session named Dedukti_$theory for ea
         //if (verbose) progress.echo("Started at " + Build_Log.print_date(start_date) + "\n")
 
         progress.interrupt_handler {
-          try rootfile(options, session, progress, dirs, verbose)
+          try rootfile(options, session, theory, progress, dirs, verbose)
           catch {case x: Exception =>
             progress.echo(x.getStackTrace.mkString("\n"))
             println(x)}
